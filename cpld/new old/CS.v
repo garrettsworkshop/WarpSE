5e958a385b6401235a3c381fccf5acc22cc4d007
Zane Kaminski <zanekaminski@gmail.com>
2024-10-03 07:53:59 -0400

    Add "new old"

---
+++ cpld/new old/CS.v
@@ -0,0 +1,70 @@
+module CS(
+	/* MC68HC000 interface */
+	input [23:08] A, input CLK, input nRES, input nWE, 
+	/* AS cycle detection */
+	input BACT,
+	/* QoS enable input */
+	input IOQoSEN,
+	/* Device select outputs */
+	output IOCS, output IORealCS, output IOPWCS, output IACKCS,
+	output ROMCS, output ROMCS4X,
+	output RAMCS, output RAMCS0X,
+	output IOQoSCS, output SndQoSCS);
+
+	/* Overlay control */
+	reg Overlay;
+	always @(posedge CLK) begin
+		if (!BACT && !nRES) Overlay <= 1;
+		else if (BACT && ROMCS4X) Overlay <= 0;
+	end
+
+	/* I/O select signals */
+	assign IACKCS = A[23:20]==4'hF;
+	wire VIACS = A[23:20]==4'hE;
+	wire IWMCS = A[23:20]==4'hD;
+	wire SCCCS = A[23:20]==4'hB || A[23:20]==4'h9;
+	wire SCSICS = A[23:20]==4'h5;
+
+	/* ROM select signals */
+	assign ROMCS4X = A[23:20]==4'h4;
+	assign ROMCS = Overlay || ROMCS4X;
+
+	/* RAM select signals */
+	assign RAMCS0X = A[23:22]==2'b00;
+	assign RAMCS = RAMCS0X && !Overlay;
+	wire VidRAMCSWR64k = A[23:16]==8'h3F && !nWE; // 3F0000-3FFFFF
+	wire VidRAMCSWR = VidRAMCSWR64k; //&& (
+		//A[15:12]==4'h2 || // 1792 bytes RAM, 2304 bytes video
+		//A[15:12]==4'h3 || // 4096 bytes video
+		//A[15:12]==4'h4 || // 4096 bytes video
+		//A[15:12]==4'h5 || // 4096 bytes video
+		//A[15:12]==4'h6 || // 4096 bytes video
+		//A[15:12]==4'h7 || // 3200 bytes video, 896 bytes RAM
+		//A[15:12]==4'hA || // 256 bytes RAM, 768 bytes sound, 768 bytes RAM, 2304 bytes video
+		//A[15:12]==4'hB || // 4096 bytes video
+		//A[15:12]==4'hC || // 4096 bytes video
+		//A[15:12]==4'hD || // 4096 bytes video
+		//A[15:12]==4'hE || // 4096 bytes video
+		//A[15:12]==4'hF); // 3200 bytes video, 128 bytes RAM (system error space), 768 bytes sound
+	assign SndQoSCS = VidRAMCSWR64k && (
+		((A[15:12]==4'hF) && (A[11:8]==4'hD || A[11:8]==4'hE || A[11:8]==4'hF)) ||
+		((A[15:12]==4'hA) && (A[11:8]==4'h1 || A[11:8]==4'h2 || A[11:8]==4'h3)));
+	assign IOQoSCS =  IWMCS || VIACS || SCCCS || SCSICS;
+
+	/* Select signals - IOB domain */
+	assign IACKCS = A[23:20]==4'hF; // IACK
+	assign IORealCS =
+		A[23:20]==4'hF || // IACK
+		A[23:20]==4'hE || // VIA
+		A[23:20]==4'hD || // IWM
+		A[23:20]==4'hC || // empty / fast ROM
+		A[23:20]==4'hB || // SCC write
+		A[23:20]==4'hA || // empty
+		A[23:20]==4'h9 || // SCC read/reset
+		A[23:20]==4'h8 || // empty (expansion RAM)
+		A[23:20]==4'h7 || // empty (expansion RAM)
+		A[23:20]==4'h6 || // empty (expansion RAM)
+		A[23:20]==4'h5;   // SCSI
+	assign IOCS = IORealCS || VidRAMCSWR || IOQoSEN;
+	assign IOPWCS = VidRAMCSWR64k && !IOQoSEN; // Posted write to video RAM only when QoS disabled
+endmodule
---
+++ cpld/new old/IOBM.v
@@ -0,0 +1,140 @@
+module IOBM(
+	/* PDS interface */
+	input C16M, input C8M, input E,
+	output reg nAS, output reg nLDS, output reg nUDS, output reg RnW, output reg nVMA,
+	input nDTACK, input nVPA, input nBERR, input nRES,
+	/* PDS address and data latch control */
+	input AoutOE, output nDoutOE, output reg ALE0, output reg nDinLE,
+	/* IO bus slave port interface */
+	input IOREQ, input IORW, input IOLDS, input IOUDS,
+	output reg IOACT, output reg IODONE, output reg IOBERR);
+
+	/* C8M clock registration */
+	reg C8Mr; always @(posedge C16M) C8Mr <= C8M;
+
+	/* I/O request input synchronization */
+	reg IOREQr; always @(posedge C16M) IOREQr <= IOREQ;
+
+	/* VPA synchronization */
+	reg VPAr; always @(negedge C8M) VPAr <= !nVPA;
+	
+	/* E clock synchronization */
+	reg Er; always @(negedge C8M) begin Er <= E; end
+	
+	/* E clock state */
+	reg [3:0] ES;
+	always @(negedge C8M) begin
+		if (!E && Er) ES <= 1;
+		else if (ES==0 || ES==9) ES <= 0;
+		else ES <= ES+1;
+	end
+
+	/* ETACK and VMA generation */
+	wire ETACK = (ES==8) && !nVMA;
+	always @(posedge C8M) begin
+		if ((ES==5) && IOACT && VPAr) nVMA <= 0;
+		else if(ES==0) nVMA <= 1;
+	end
+	
+	/* DTACK and BERR synchronization */
+	always @(negedge C8M, posedge nAS) begin
+		if (nAS) begin
+			IODONE <= 0;
+			IOBERR <= 0;
+		end else begin
+			IODONE <= (!nDTACK || ETACK || !nRES);
+			IOBERR <= !nBERR;
+		end
+	end
+
+	/* I/O bus state */
+	reg [2:0] IOS = 0;
+	reg IOS0;
+	always @(posedge C16M) case (IOS[2:0])
+		3'h0: begin
+			if (IOREQr && !C8Mr && AoutOE) begin // "IOS1"
+				IOS <= 2;
+				IOS0 <= 0;
+			end else begin // "regular" IOS0
+				IOS <= 0;
+				IOS0 <= 1;
+			end
+			IOACT <= IOREQr;
+			ALE0 <= IOREQr;
+		end 3'h2: begin
+			IOS <= 3;
+			IOS0 <= 0;
+			IOACT <= 1;
+			ALE0 <= 1;
+		end 3'h3: begin
+			IOS <= 4;
+			IOS0 <= 0;
+			IOACT <= 1;
+			ALE0 <= 1;
+		end 3'h4: begin
+			IOS <= 5;
+			IOS0 <= 0;
+			IOACT <= 1;
+			ALE0 <= 1;
+		end 3'h5: begin
+			if (!C8Mr && (IODONE || IOBERR)) begin
+				IOS <= 6;
+				IOACT <= 0;
+			end else begin
+				IOS <= 5;
+				IOACT <= 1;
+			end
+			IOS0 <= 0;
+			ALE0 <= 1;
+		end 3'h6: begin
+			IOS <= 7;
+			IOS0 <= 0;
+			IOACT <= 0;
+			ALE0 <= 0;
+		end 3'h7: begin
+			IOS <= 0;
+			IOS0 <= 1;
+			IOACT <= 0;
+			ALE0 <= 0;
+		end
+	endcase
+
+	/* PDS address and data latch control */
+	always @(negedge C16M) begin nDinLE = IOS==4 || IOS==5; end
+	reg DoutOE = 0;
+	always @(posedge C16M) begin
+		DoutOE <= (IOS==0 && IOREQr && !IORW && !C8Mr) ||
+				  (DoutOE && (IOS==2 || IOS==3 || IOS==4 || IOS==5));
+	end
+	assign nDoutOE = !(AoutOE && (DoutOE || (IOS0 && !IOREQr)));
+
+	/* AS, DS, RW control */
+	always @(negedge C16M) begin
+		nAS <= !(
+			(IOS==0 && IOREQr && !C8Mr) || 
+			(IOS==2) || 
+			(IOS==3) || 
+			(IOS==4) || 
+			(IOS==5));
+		RnW <= !(
+			(IOS==0 && IOREQr && !IORW && !C8Mr) || 
+			(!IORW && IOS==2) || 
+			(!IORW && IOS==3) || 
+			(!IORW && IOS==4) || 
+			(!IORW && IOS==5) || 
+			(!IORW && IOS==6));
+		nLDS <= !(
+			(IOS==0 && IOREQr && IORW && IOLDS && !C8Mr) ||
+			(IOS==2 && IOLDS) ||
+			(IOS==3 && IOLDS) ||
+			(IOS==4 && IOLDS) ||
+			(IOS==5 && IOLDS));
+		nUDS <= !(
+			(IOS==0 && IOREQr && IORW && IOUDS && !C8Mr) ||
+			(IOS==2 && IOUDS) ||
+			(IOS==3 && IOUDS) ||
+			(IOS==4 && IOUDS) ||
+			(IOS==5 && IOUDS));
+	end
+
+endmodule
